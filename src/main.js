76886a9f23c72f8562370175b045f809d63697b1
DiceMasters <lidef@yandex.ru>
2018-06-01 02:28:47 +0500

    Все сверстано и запилено на отличненько. Один костыль присутствует. Макет не адаптивен.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,48 @@
 $(document).ready( function(){
 
+	/*
+	*
+	* Добавление в корзину
+	*
+	*/
+
+	var min = 1;
+
+	$('.good').children('button').click( function(){
+
+		var caption = $(this).siblings('h3').html()
+		var price = $(this).siblings('.price').html()
+		var articul = $(this).parent('div.good').attr('articul')
+		var curent = min++;
+		console.log('Articul = ' + articul)
+
+		var template = "\
+			<div class='addToCart' lightbox='" + curent + "' id='needtoremove' style='display:none'>\
+				<div class='topCart'>\
+					<p>Товар добавлен в корзину!</p>\
+					<button class='lightcloseButton' lightclose='" + curent + "'><i class='fa fa-times' aria-hidden='true'></i></button>\
+				</div>\
+				<p>" + caption + "</p>\
+				<div class='downCart'>\
+					<p>" + price + "</p>\
+					<a href='#'>Перейти в корзину</a>\
+				</div>\
+			</div>\
+		"
+
+		if ( $('.addToCart').length >= 4 ) {
+			var div = document.getElementById('needtoremove')
+			document.getElementById('heaven').removeChild(div);
+		}
+
+		$('#heaven').append(template)
+		$('div[lightbox = ' + curent + ']').fadeIn(250)
+
+	})
+
+	$('body').on('click', '.lightcloseButton', function(e) {
+		$(this).closest('.addToCart').remove()
+	})
 
 
 	/*
@@ -35,5 +78,25 @@ $(document).ready( function(){
 		}
 
 	})
+
+	// Плавающий Label
+	$('.floatLabel > input').focusin( function(){
+
+		$(this).siblings('label').css({'top':'-14px','transition-duration':'0.25s'})
+
+	})
+
+	$('.floatLabel > input').focusout( function(){
+
+		if ( $(this).val() == '' ) {
+			$(this).siblings('label').css({'top':'7px','transition-duration':'0.25s'})
+		}
+		else{
+			$(this).siblings('label').css({'top':'-14px','transition-duration':'0.25s'})
+		}
+
+	})
+
+
 
 })
